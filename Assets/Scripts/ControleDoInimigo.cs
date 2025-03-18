69b68a4557540492edb1fd40c7a90537c3e36f73
Léo Davis <leo.davis@outlook.com>
2025-03-18 19:05:46 -0300

    Primeiro commit

---
+++ Assets/Scripts/ControleDoInimigo.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDoInimigo : MonoBehaviour
+{
+    [Header("Referencias Gerais")]
+    private Rigidbody2D oRigidbody2D;
+    private Animator oAnimator;
+    private GameObject oJogador;
+
+    [Header("Movimento do Inimigo")]
+    [SerializeField] private float velocidadeDoInimigo;
+    private Vector2 direcaoDoMovimento;
+
+    [Header("Ataque do Inimigo")]
+    [SerializeField]private float distanciaParaAtacar;
+
+    private void Start()
+    {
+        oRigidbody2D = GetComponent<Rigidbody2D>();
+        oAnimator = GetComponent<Animator>();
+        oJogador = Object.FindAnyObjectByType<ControleDoJogador>().gameObject;
+    }
+
+    private void Update()
+    {
+        EspelharInimigo();
+        SeguirJogador();
+    }
+
+    private void EspelharInimigo()
+    {
+        //Faz o inimigo olhar na direção do Jogador(Esquerda / Direita)
+        if (oJogador.transform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(oJogador.transform.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(1f , 1f, 1f);
+        }
+    
+    }   
+
+    private void SeguirJogador()
+    {   //Armazena a posição do Jogador e se movimenta a ele
+        if(Vector2.Distance(transform.position, oJogador.transform.position) > distanciaParaAtacar)
+        {
+        direcaoDoMovimento = (oJogador.transform.position - transform.position).normalized;
+        oRigidbody2D.linearVelocity = direcaoDoMovimento * velocidadeDoInimigo;
+
+        oAnimator.SetTrigger("andando");
+        }
+        // Deixa de se movimentar e ataca o Jogador
+        else
+        {
+            oRigidbody2D.linearVelocity = Vector2.zero;
+
+            oAnimator.SetTrigger("parado");
+        }
+    }
+}
---
+++ Assets/Scripts/ControleDoJogador.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ControleDoJogador : MonoBehaviour
+{
+    [Header("Referências Gerais")]
+    private Rigidbody2D oRigidbody2D;
+    private Animator oAnimator;
+
+    [Header("Movimento do Jogador")]
+    [SerializeField] private float velocidadeDoJogador;
+    private Vector2 inputDeMovimento;
+    private Vector2 direcaoDoMovimento;
+
+    [Header("Controle do Ataque")]
+    [SerializeField]private float tempoMaximoEntreAtaques;
+    private float tempoAtualEntreAtaques;
+    private bool podeAtacar;
+
+    private void Start()
+    {
+        oRigidbody2D = GetComponent<Rigidbody2D>();
+        oAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        RodarCronometroDosAtaques();
+        ReceberInputs();
+        RodarAnimacoesEAtaques();
+        EspelharJogador();
+        MovimentarJogador();
+    }
+    
+    //Controla o tempo entre os Ataques
+    private void RodarCronometroDosAtaques()
+    {
+        tempoAtualEntreAtaques -= Time.deltaTime;
+        if(tempoAtualEntreAtaques <= 0)
+        {
+            podeAtacar = true;
+            tempoAtualEntreAtaques = tempoMaximoEntreAtaques;
+        }
+    }
+
+    private void ReceberInputs()
+    {
+        //Armazena a direção que o Jogador quer seguir
+        inputDeMovimento = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void RodarAnimacoesEAtaques()
+    {   //Rodam a Movimentação do Jogador
+        if(inputDeMovimento.magnitude == 0)
+        {
+            oAnimator.SetTrigger("parado");
+        }
+        else if(inputDeMovimento.magnitude != 0)
+        {
+            oAnimator.SetTrigger("andando");
+        }
+        //Rodam os Ataques do Jogador
+        if(Input.GetKeyDown(KeyCode.J) && podeAtacar)
+        {
+            oAnimator.SetTrigger("socando");
+            podeAtacar = false;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.K) && podeAtacar)
+        {
+            oAnimator.SetTrigger("chutando");
+            podeAtacar = false;
+        }
+    }
+
+    private void EspelharJogador()
+    {
+        //Faz o jogador olhar para a direção que está andando(direita / esquerda)
+        if(inputDeMovimento.x == 1)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(inputDeMovimento.x == -1)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    private void MovimentarJogador()
+    {
+        //Movimenta o Jogador com base na sua direção
+        direcaoDoMovimento = inputDeMovimento.normalized;
+        oRigidbody2D.linearVelocity = direcaoDoMovimento * velocidadeDoJogador;
+    }
+}
