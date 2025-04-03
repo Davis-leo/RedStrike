62ec0381e0ca674f899f7cd04fb6dc07e17dc893
Léo Davis <leo.davis@outlook.com>
2025-04-02 22:07:49 -0300

    Prog. a barra de vida do Inimigo

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [SerializeField]private GameObject painelDoInimigo;
+    [SerializeField]private Slider barraDeVidaDoInimigoAtual;
+    [SerializeField]private TMP_Text textoDoNomeDoInimigoAtual;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        DesativarPainelDoInimigo();
+    }
+
+    public void AtivarPainelDoInimigo()
+    {
+        painelDoInimigo.SetActive(true);
+    }
+
+    public void DesativarPainelDoInimigo()
+    {
+        painelDoInimigo.SetActive(false);
+    }
+
+    public void AtualizarBarraDeVidaDoInimigoAtual(int valorMaximo, int valorAtual, string nomeDoInimigo)
+    {
+        barraDeVidaDoInimigoAtual.maxValue = valorMaximo;
+        barraDeVidaDoInimigoAtual.value = valorAtual;
+
+        textoDoNomeDoInimigoAtual.text = nomeDoInimigo;
+
+        AtivarPainelDoInimigo();
+    }
+}
--- Assets/Scripts/VidaDoInimigo.cs
+++ Assets/Scripts/VidaDoInimigo.cs
@@ -7,6 +7,9 @@ public class VidaDoInimigo : MonoBehaviour
     [Header("Verificações")]
     public bool inimigoVivo;
 
+    [Header("Parâmetros")]
+    [SerializeField]private string nomeDoInimigo;
+
     [Header("Controle da Vida")]
     [SerializeField]private int vidaMaxima;
     private int vidaAtual;
@@ -30,12 +33,14 @@ public class VidaDoInimigo : MonoBehaviour
         {
             vidaAtual -= danoParaReceber;
             GetComponent<ControleDoInimigo>().RodarAnimacaoDeDano();
+            UIManager.instance.AtualizarBarraDeVidaDoInimigoAtual(vidaMaxima, vidaAtual, nomeDoInimigo);
             
             if(vidaAtual <= 0)
             {   
                 inimigoVivo = false;
                 GetComponent<ControleDoInimigo>().RodarAnimacaoDeDerrota();
                 SpawnarComida();
+                UIManager.instance.DesativarPainelDoInimigo();
                 Destroy(this.gameObject, tempoParaSumir);
             }
         }
